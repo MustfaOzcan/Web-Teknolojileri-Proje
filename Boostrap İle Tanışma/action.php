934a4b2a472178992cf557c5cd9a551ed379531f
Mustafa Özcan <103693735+MustfaOzcan@users.noreply.github.com>
2022-05-04 01:53:06 +0300

    Login Sayfası Eklendi
    >Login Eklendi 
    >Domanin alındı
    >Hostinge Dosyalar Yüklendi

---
+++ Boostrap İle Tanışma/action.php
@@ -0,0 +1,29 @@
+<?php
+
+$eposta = "g201210090@sakarya.edu.tr";
+$sifre = "g201210090";
+
+
+if(($_POST['e-posta']==$eposta) && ($_POST['sifre']==$sifre) )
+{
+
+    echo " <h1> HOŞGELDİN,</h1>";
+    echo " <h1><i> g201210090</i>";
+    echo "<h2><a  href ='https:google.com'>Ana Sayfa</a></h2>";
+
+    
+}
+else{
+    
+
+    echo "<h1> HATALI GİRİŞ</h1>";
+    echo "<h2><a  href ='http://localhost/Login/login.html'>Geri Dön</a></h2>";
+   
+
+}
+
+
+
+?>
+
+
